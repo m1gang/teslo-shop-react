6322292cc0fc0d50b86a20f2feae73111f891e48
MiGaNg <miguelangelyv1@gmail.com>
2025-11-12 12:10:48 -0500

    HomePage - Paginación y productos

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,14 @@
+import { CustomPagination } from '@/components/custom/CustomPagination'
+import { products } from '@/mocks/products.mock'
 import { CustomJumbotron } from '@/shop/components/CustomJumbotron'
+import { ProductsGrid } from '@/shop/components/ProductsGrid'
 
 export const HomePage = () => {
     return (
         <div>
             <CustomJumbotron title='Todos los productos' />
+            <ProductsGrid products={products} />
+            <CustomPagination totalPages={8} />
         </div>
     )
 }
