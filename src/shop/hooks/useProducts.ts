a42e33bd2526d75df859d3eb35906e030fdd80e0
MiGaNg <miguelangelyv1@gmail.com>
2025-11-26 12:02:37 -0500

    Filtrar por rango de precios

--- src/shop/hooks/useProducts.ts
+++ src/shop/hooks/useProducts.ts
@@ -10,18 +10,45 @@ export const useProducts = () => {
 
     const limit = searchParams.get('limit') || 9;
     const page = searchParams.get('page') || 1;
-    const sizes = searchParams.get('sizes');
+    const sizes = searchParams.get('sizes') || undefined;
 
-    console.log({ sizes, gender })
     const offset = (Number(page) - 1) * Number(limit);
 
+    const price = searchParams.get('price') || 'any';
+    let minPrice = undefined;
+    let maxPrice = undefined;
+
+    switch (price) {
+        case 'any':
+            break;
+        case '0-50':
+            minPrice = 0;
+            maxPrice = 50;
+            break;
+        case '50-100':
+            minPrice = 50;
+            maxPrice = 100;
+            break;
+        case '100-200':
+            minPrice = 100;
+            maxPrice = 200;
+            break;
+        case '200+':
+            minPrice = 200;
+            maxPrice = undefined;
+            break;
+    }
+
+
     return useQuery({
-        queryKey: ['products', { offset, limit, sizes, gender }],
+        queryKey: ['products', { offset, limit, sizes, gender, minPrice, maxPrice }],
         queryFn: () => getProductsAction({
             limit: isNaN(+limit) ? 9 : limit,
             offset: isNaN(offset) ? 0 : offset,
             sizes,
-            gender
+            gender,
+            minPrice,
+            maxPrice
         }),
         staleTime: 1000 * 60 * 5
     })
