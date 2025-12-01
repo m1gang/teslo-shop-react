a8901177ad86cecc9c93831b70524941524f407d
MiGaNg <miguelangelyv1@gmail.com>
2025-12-01 11:13:02 -0500

    Gestor de estado - Zustand

--- src/shop/pages/product/ProductPage.tsx
+++ src/shop/pages/product/ProductPage.tsx
@@ -1,7 +1,21 @@
+import { useCounterStore } from '@/auth/store/auth.store'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export const ProductPage = () => {
+    const { inc, count, dec, incBy } = useCounterStore();
     return (
-        <div>ProductPage</div>
+        <>
+            <h1 className='text-3xl font-montserrat'>Count: {count}</h1>
+            <Button onClick={inc}>
+                +1
+            </Button>
+            <Button onClick={dec}>
+                -1
+            </Button>
+            <Button onClick={() => incBy(5)}>
+                +5
+            </Button>
+        </>
     )
 }
