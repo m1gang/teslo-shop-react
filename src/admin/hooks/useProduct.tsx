76e583295b399bd88c2453f70a9aec7b4787ddd1
MiGaNg <miguelangelyv1@gmail.com>
2026-01-07 11:41:14 -0500

    Crear producto

--- src/admin/hooks/useProduct.tsx
+++ src/admin/hooks/useProduct.tsx
@@ -1,10 +1,12 @@
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { getProductByIdAction } from "../actions/get-product-by-id.action"
 import type { Product } from "@/interfaces/product.interface"
 import { create } from "zustand"
 import { createUpdateProductAction } from "../actions/create-update-product.action"
 
 export const useProduct = (id: string) => {
+
+    const queryClient = useQueryClient();
 
     const query = useQuery({
         queryKey: ['product', { id }],
@@ -16,9 +18,11 @@ export const useProduct = (id: string) => {
     const mutation = useMutation({
         mutationFn: createUpdateProductAction,
         onSuccess: (product: Product) => {
-            console.log('todo salió bien', product);
-            //invalidar cache
+            //invalidar caché
+            queryClient.invalidateQueries({ queryKey: ['products'] });
+            queryClient.invalidateQueries({ queryKey: ['product', { id: product.id }] });
             //actualizar querydata
+            queryClient.setQueryData(['products', { id: product.id }], product);
         }
     });
 
